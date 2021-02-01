5412db8d51a0aa836e266811dbea18b8c6a12f49
Calvin L <71704050+Kalv1@users.noreply.github.com>
2021-02-01 14:23:19 +0100

    Création de la structure du projet.

---
+++ monde/Activite.java
@@ -0,0 +1,19 @@
+package twisk.monde;
+
+public class Activite extends Etape {
+    public int temps;
+    int ecartTemps;
+
+    public Activite(String nom) {
+        super(nom);
+    }
+
+    public Activite(String nom, int t, int e){
+        super(nom);
+    }
+
+    public boolean estUneActivite(){
+        return true;
+    }
+
+}
---
+++ monde/Monde.java
@@ -0,0 +1,35 @@
+package twisk.monde;
+
+import java.util.Iterator;
+
+public class Monde implements Iterable {
+
+    public Monde(){
+
+    }
+
+    void aCommeEntree(Etape... etapes){
+
+    }
+
+    void aCommeSortie(Etape... etapes){
+
+    }
+
+    void ajouter(Etape... etapes){
+
+    }
+
+    int nbEtapes(){
+        return 0;
+    }
+
+    int nbGuichets(){
+        return 0;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+}
