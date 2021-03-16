1b5a477217e8bec8f56b179e14e73ab0b29523c5
Calvin L <71704050+Kalv1@users.noreply.github.com>
2021-03-16 16:47:46 +0100

    Ajout des fonctions native + compilations.

--- src/twisk/simulation/Simulation.java
+++ src/twisk/simulation/Simulation.java
@@ -11,6 +11,13 @@ public class Simulation {
         environnement = new KitC();
     }
 
+
+    public native int[] start_simulation();
+
+    public native int[] ou_sont_les_clients();
+
+    public native void nettoyage();
+
     public void simuler(Monde monde) {
         environnement.creerFichier(monde.toC().toString());
         environnement.compiler();
