debcff910dee630992bd4db6966ccdd57093add0
Hugo Mathieu-Steinbach <hugo.mathieu-steinbach5@etu.univ-lorraine.fr>
2021-05-27 09:11:24 +0200

    Réparation des entrées multiples.

--- src/twisk/monde/SasEntree.java
+++ src/twisk/monde/SasEntree.java
@@ -22,8 +22,13 @@ public class SasEntree extends Activite {
      * @return Un string du code C de sasEntree
      */
     public String toC() {
-        return "entrer(" + getNom().replaceAll("\\s+", "") + "); \n" +
-                "delai(" + 3 + "," + 1 + "); \n" +
-                "transfert(" + getNom().replaceAll("\\s+", "") + "," + getSuivant().getNom().replaceAll("\\s+", "") + ");\n" + getSuivant().toC();
+        StringBuilder strC = new StringBuilder();
+        strC.append("entrer(").append(getNom().replaceAll("\\s+", "")).append("); \n").append("delai(").append(3).append(",").append(1).append("); \n");
+        strC.append("delai(").append(getTemps()).append(",").append(getEcartTemps()).append(");\n").append("int nb = (int)((rand()/(float) RAND_MAX)*").append(getGestionnaireSuccesseurs().nbEtapes()).append(");\n").append("switch(nb) {\n");
+        for(int i = 0; i < getGestionnaireSuccesseurs().nbEtapes(); ++i) {
+            strC.append("case ").append(i).append(": {").append("transfert(").append(getNom().replaceAll("\\s+", "")).append(",").append(getGestionnaireSuccesseurs().getEtapes().get(i).getNom().replaceAll("\\s+", "")).append(");\n").append(getGestionnaireSuccesseurs().getEtapes().get(i).toC()).append("break;\n}\n");
+        }
+        strC.append("}");
+        return strC.toString();
     }
 }
